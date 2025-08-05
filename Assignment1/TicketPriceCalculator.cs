debf4a75fdaf83d9ef78a4ea0ce46542b8f7463e
Zheena26 <zibrahim018@st.ug.edu.gh>
2025-08-05 09:39:23 +0000

    committed TicketPriceIdentifier.cs

---
+++ Assignment1/TicketPriceCalculator.cs
@@ -0,0 +1,24 @@
+﻿class TicketPriceCalculator
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        string? input = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("Invalid input. Please enter a valid age.");
+            return;
+        }
+
+        int age = int.Parse(input);
+        if (age < 0)
+        {
+            Console.WriteLine("Invalid age. Age cannot be negative.");
+            return;
+        }
+        if (age <= 12 || age >= 65)
+            Console.WriteLine("Ticket Price: GHC7");
+        else
+            Console.WriteLine("Ticket Price: GHC10");
+    }
+}
